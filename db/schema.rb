e3157140f3f22517c6304d1f033a108f880de0bd
Tommy5230 <tomasz.donarski@naturaily.com>
2021-07-29 10:12:57 +0200

    Add price attribute to tickets, supplement seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_175553) do
+ActiveRecord::Schema.define(version: 2021_07_29_080731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_07_28_175553) do
     t.boolean "sold", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price", default: 0, null: false
     t.index ["event_id"], name: "index_tickets_on_event_id"
   end
 

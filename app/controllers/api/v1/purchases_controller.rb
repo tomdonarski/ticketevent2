c6ecd9aa3d6813295c0ac3aeb2a2729307f38d5d
Tommy5230 <tomasz.donarski@naturaily.com>
2021-08-03 23:22:10 +0200

    WIP

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -3,14 +3,14 @@ module Api
     class PurchasesController < ApplicationController
       deserializable_resource :purchase, only: [:create]
 
-      def create(service = CreatePurchaseService.new)
-        service.call(find_ticket, purchase_params)
+      def create(service = CreatePurchaseService.new(find_ticket, purchase_params))
+        service.call
       end
 
       private
 
       def purchase_params
-        params.require(:purchase).permit(:ticket_id)
+        params.require(:purchase).permit(:ticket_id, :card_number)
       end
 
       def find_ticket
